e010f8d621aef257b6b71ee7e419514cc7dc5819
87ouo <huayexin@aliyun.com>
2024-02-13 22:42:27 +0800

    预发版 - ver1.0.0.0213

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+import datetime
 import json
+import logging
 import os
 import re
 import subprocess
+from time import sleep
 
 import requests
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+now = datetime.datetime.now()
+formatted_time = now.strftime("%Y%m%d_%H%M%S.%f")[:-3]
+exe_path = os.path.split(os.path.realpath(__file__))[0]
+log_file_path = os.path.join(exe_path, 'logs')
+if not os.path.exists(log_file_path):
+    os.makedirs(log_file_path)
+log_file_path = os.path.join(log_file_path, "UTC+8 " + formatted_time + '.log')
+handler = logging.FileHandler(log_file_path)
+handler.setLevel(logging.DEBUG)
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+console.setFormatter(formatter)
+logger.addHandler(handler)
+logger.addHandler(console)
 
 
 def is_valid_domain(domain: str) -> bool:
@@ -28,7 +50,7 @@ def load_cfg_json():
                 fw.write(cfg_file_content_original)
                 fw.close()
     except Exception as e:
-        print(e)
+        logger.warning('Creating Default Config File Failed --- ' + str(e))
         is_ok_flag = "Creating Default Config File Failed"
 
     try:
@@ -41,11 +63,11 @@ def load_cfg_json():
                         fw.write(cfg_file_content_original)
                         fw.close()
                 except Exception as e:
-                    print(e)
+                    logger.warning('Writing Default Config Content Failed --- ' + str(e))
                     is_ok_flag = "Writing Default Config Content Failed"
             fr.close()
     except Exception as e:
-        print(e)
+        logger.warning('Read Config File Failed --- ' + str(e))
         is_ok_flag = "Read Config File Failed"
 
     api_key, target_url_v4, zone_id, now_ipv4_address, now_ipv6_address, sleep_time_in_ms = "", "", "", "", "", 0
@@ -65,24 +87,28 @@ def load_cfg_json():
 
             if api_key == "":
                 is_ok_flag = "Read Config File Failed - No API key"
-                print(is_ok_flag)
+                logger.warning(is_ok_flag)
+
             elif zone_id == "":
                 is_ok_flag = "Read Config File Failed - No zone id"
-                print(is_ok_flag)
+                logger.warning(is_ok_flag)
+
             elif target_url_v4 == "":
                 is_ok_flag = "Read Config File Failed - No IPv4 target url"
-                print(is_ok_flag)
+                logger.warning(is_ok_flag)
             elif is_valid_domain(target_url_v4) == False:
                 is_ok_flag = "Read Config File Failed - IPv4 Target URL Not Valid"
-                print(is_ok_flag)
+                logger.warning(is_ok_flag)
+
             elif target_url_v6 == "":
                 is_ok_flag = "Read Config File Failed - No IPv6 target url"
-                print(is_ok_flag)
+                logger.warning(is_ok_flag)
             elif is_valid_domain(target_url_v6) == False:
                 is_ok_flag = "Read Config File Failed - IPv6 Target URL Not Valid"
-                print(is_ok_flag)
+                logger.warning(is_ok_flag)
+
     except Exception as e:
-        print(e)
+        logger.warning('Read Config File Failed --- ' + str(e))
         is_ok_flag = "Read Config File Failed"
 
     return is_ok_flag, api_key, zone_id, target_url_v4, target_url_v6, now_ipv4_address, now_ipv6_address, sleep_time_in_ms
@@ -100,7 +126,7 @@ def save_cfg_json(new_ipv4: str, new_ipv6: str):
             cfg_json_item = json.loads(cfg_file_content)
             fr.close()
     except Exception as e:
-        print(e)
+        logger.warning('Read Config File Failed --- ' + str(e))
         is_ok_flag = "Read Config File Failed"
 
     cfg_json_item['now_ipv4_address'] = new_ipv4
@@ -108,11 +134,11 @@ def save_cfg_json(new_ipv4: str, new_ipv6: str):
 
     try:
         with open(cfg_file_path, 'w') as fw:
-            cfg_file_content = cfg_json_item.dumps()
+            cfg_file_content = json.dumps(cfg_json_item)
             fw.write(cfg_file_content)
             fw.close()
     except Exception as e:
-        print(e)
+        logger.warning('Write Config File Failed --- ' + str(e))
         is_ok_flag = "Write Config File Failed"
 
     return is_ok_flag
@@ -140,6 +166,9 @@ def get_dns_record_list_from_cloudflare(zone_id: str, api_key: str):
     }
     response = requests.request("GET", url, headers=headers)
     res_json_item = json.loads(response.text)
+
+    logger.info(response.text)
+
     return res_json_item
 
 
@@ -148,28 +177,162 @@ def check_is_need_create_or_update_in_clodflare_dns_record(target_url_v4: str, n
                                                            res_json_item):
     is_v4_target_url_existed = False
     is_ipv4_need_update = False
+    ipv4_dns_record_id = ""
+
     is_v6_target_url_existed = False
     is_ipv6_need_update = False
+    ipv6_dns_record_id = ""
 
     for i in res_json_item['result']:
-        if i['name'] == target_url_v4:
-            is_v4_target_url_existed = True
-            if i['type'] == "A":
-                if i['content'] != now_ipv4_address:
-                    is_ipv4_need_update = True
+        if target_url_v4 != "" and now_ipv4_address != "":
+            if i['name'] == target_url_v4:
+                is_v4_target_url_existed = True
+                if i['type'] == "A":
+                    if i['content'] != now_ipv4_address:
+                        is_ipv4_need_update = True
+                        ipv4_dns_record_id = i['id']
 
-        elif i['name'] == target_url_v6:
-            is_v6_target_url_existed = True
-            if i['type'] == "AAAA":
-                if i['content'] != now_ipv6_address:
-                    is_ipv6_need_update = True
+        if target_url_v6 != "" and now_ipv6_address != "":
+            if i['name'] == target_url_v6:
+                is_v6_target_url_existed = True
+                if i['type'] == "AAAA":
+                    if i['content'] != now_ipv6_address:
+                        is_ipv6_need_update = True
+                        ipv6_dns_record_id = i['id']
 
-    return is_v4_target_url_existed, is_ipv4_need_update, is_v6_target_url_existed, is_ipv6_need_update
+    return is_v4_target_url_existed, is_ipv4_need_update, ipv4_dns_record_id, is_v6_target_url_existed, is_ipv6_need_update, ipv6_dns_record_id
+
+
+def create_dns_record_for_cloudflare(ipv4_or_ipv6: int, target_url: str, now_address: str, api_key: str, zone_id: str):
+    is_ok_flag = "OK"
+
+    url = "https://api.cloudflare.com/client/v4/zones/" + zone_id + "/dns_records"
+    payload = {}
+    if ipv4_or_ipv6 == 4:
+        payload = {
+            "content": now_address,
+            "name": target_url,
+            "proxied": False,
+            "type": "A",
+            "comment": "",
+            "tags": [],
+            "ttl": 1
+        }
+    elif ipv4_or_ipv6 == 6:
+        payload = {
+            "content": now_address,
+            "name": target_url,
+            "proxied": False,
+            "type": "AAAA",
+            "comment": "",
+            "tags": [],
+            "ttl": 1
+        }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + api_key
+    }
+
+    try:
+        response = requests.request("POST", url, json=payload, headers=headers)
+        logger.info(response.text)
+    except Exception as e:
+        logger.warning('Create DNS Record Failed --- ' + str(e))
+        is_ok_flag = "Create DNS Record Failed"
+
+    return is_ok_flag
+
+
+def update_dns_record_for_cloudflare(ipv4_or_ipv6: int, target_url: str, target_dns_record_id: str, now_address: str,
+                                     api_key: str, zone_id: str):
+    is_ok_flag = "OK"
+
+    url = "https://api.cloudflare.com/client/v4/zones/" + zone_id + "/dns_records/" + target_dns_record_id
+
+    payload = {}
+
+    if ipv4_or_ipv6 == 4:
+        payload = {
+            "content": now_address,
+            "name": target_url,
+            "proxied": False,
+            "type": "A",
+            "comment": "",
+            "tags": [],
+            "ttl": 1
+        }
+    elif ipv4_or_ipv6 == 6:
+        payload = {
+            "content": now_address,
+            "name": target_url,
+            "proxied": False,
+            "type": "AAAA",
+            "comment": "",
+            "tags": [],
+            "ttl": 1
+        }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + api_key
+    }
+
+    try:
+        response = requests.request("PATCH", url, json=payload, headers=headers)
+        logger.info(response.text)
+    except Exception as e:
+        logger.warning('Update DNS Record Failed --- ' + str(e))
+        is_ok_flag = "Update DNS Record Failed"
+
+    return is_ok_flag
 
 
 if __name__ == '__main__':
-    print(get_public_ip_from_ipsb(4))
-    print(get_public_ip_from_ipsb(6))
+    is_ok_flag, api_key, zone_id, target_url_v4, target_url_v6, now_ipv4_address, now_ipv6_address, sleep_time_in_ms = load_cfg_json()
 
-    # get_dns_record_list_from_cloudflare(zone_id="f8c97397f790aa4d0dd1c4046b4c1e25",
-    #                                     api_key="vRIfnKqi6Mla_G9cPwrCak3dniGR0G9tKvxQJvd4")
+    is_loop = True
+
+    if is_ok_flag != "OK":
+        logger.info(is_ok_flag)
+        is_loop = False
+
+    while is_loop:
+        logger.info("Start Local DDNS Checking Operation......")
+
+        res_json_item = get_dns_record_list_from_cloudflare(zone_id, api_key)
+
+        now_ipv4_address = get_public_ip_from_ipsb(4)
+        now_ipv6_address = get_public_ip_from_ipsb(6)
+
+        logger.info("Now IPv4 Address: " + now_ipv4_address)
+        logger.info("Now IPv6 Address: " + now_ipv6_address)
+
+        is_v4_target_url_existed, is_ipv4_need_update, ipv4_dns_record_id, is_v6_target_url_existed, is_ipv6_need_update, ipv6_dns_record_id = \
+            check_is_need_create_or_update_in_clodflare_dns_record(target_url_v4, now_ipv4_address, target_url_v6,
+                                                                   now_ipv6_address, res_json_item)
+
+        if is_v4_target_url_existed:
+            if is_ipv4_need_update:
+                logger.info("Public IPv4 Changed, Update......")
+                update_dns_record_for_cloudflare(4, target_url_v4, ipv4_dns_record_id, now_ipv4_address, api_key,
+                                                 zone_id)
+            else:
+                logger.info("Public IPv4 NOT Change, Pass......")
+        else:
+            logger.info("IPv4 DNS Record NOT Existed, Create......")
+            create_dns_record_for_cloudflare(4, target_url_v4, now_ipv4_address, api_key, zone_id)
+
+        if is_v6_target_url_existed:
+            if is_ipv6_need_update:
+                logger.info("Public IPv6 Changed, Update......")
+                update_dns_record_for_cloudflare(6, target_url_v6, ipv6_dns_record_id, now_ipv6_address, api_key,
+                                                 zone_id)
+            else:
+                logger.info("Public IPv6 NOT Change, Pass......")
+        else:
+            logger.info("IPv6 DNS Record NOT Existed, Create......")
+            create_dns_record_for_cloudflare(6, target_url_v6, now_ipv6_address, api_key, zone_id)
+
+        save_cfg_json(now_ipv4_address, now_ipv6_address)
+
+        logger.info("Operation Finished. Sleep " + str(sleep_time_in_ms / 1000) + " Sec......\n\n")
+        sleep(sleep_time_in_ms / 1000)
