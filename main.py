f6b2c8b703c529da9b92efdceea255bf8e716263
87ouo <huayexin@aliyun.com>
2024-02-15 00:40:09 +0800

    预发版 - ver1.0.2.0215 1. config.cfgjson中新增是否通过cloudflare缓存的设置项，默认为不通过     ✓ 2. 新增检测config.cfgjson中的目标URL的一级域名是否与cloudflare中获取到的一级域名匹配    ✓ 3. 删除config.cfgjson中记录当前ipv4和ipv6地址的项目    ✓ 4. 取消运行过程中更新并保存config.cfgjson的部分    ✓

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def load_cfg_json():
     try:
         if not os.path.exists(cfg_file_path):
             with open(cfg_file_path, 'w') as fw:
-                cfg_file_content_original = "{\"api_key\":\"\",\"zone_id\":\"\",\"target_url_v4\":\"\",\"target_url_v6\":\"\",\"now_ipv4_address\":\"\",\"now_ipv6_address\":\"\",\"sleep_time_in_ms\":600000}"
+                cfg_file_content_original = "{\"api_key\":\"\",\"zone_id\":\"\",\"target_url_v4\":\"\",\"target_url_v6\":\"\",\"v4_use_cloudflare_proxy\":false,\"v6_use_cloudflare_proxy\":false,\"sleep_time_in_ms\":600000}"
                 fw.write(cfg_file_content_original)
                 fw.close()
     except Exception as e:
@@ -59,7 +59,7 @@ def load_cfg_json():
             if cfg_file_content == "":
                 try:
                     with open(cfg_file_path, 'w') as fw:
-                        cfg_file_content_original = "{\"api_key\":\"\",\"zone_id\":\"\",\"target_url_v4\":\"\",\"target_url_v6\":\"\",\"now_ipv4_address\":\"\",\"now_ipv6_address\":\"\",\"sleep_time_in_ms\":600000}"
+                        cfg_file_content_original = "{\"api_key\":\"\",\"zone_id\":\"\",\"target_url_v4\":\"\",\"target_url_v6\":\"\",\"v4_use_cloudflare_proxy\":false,\"v6_use_cloudflare_proxy\":false,\"sleep_time_in_ms\":600000}"
                         fw.write(cfg_file_content_original)
                         fw.close()
                 except Exception as e:
@@ -70,7 +70,7 @@ def load_cfg_json():
         logger.warning('Read Config File Failed --- ' + str(e))
         is_ok_flag = "Read Config File Failed"
 
-    api_key, target_url_v4, zone_id, now_ipv4_address, now_ipv6_address, sleep_time_in_ms = "", "", "", "", "", 0
+    api_key, target_url_v4, zone_id, v4_use_cloudflare_proxy, v6_use_cloudflare_proxy, sleep_time_in_ms = "", "", "", False, False, 0
 
     try:
         with (open(cfg_file_path, 'r') as fr):
@@ -81,8 +81,12 @@ def load_cfg_json():
             zone_id = cfg_json_item['zone_id']
             target_url_v4 = cfg_json_item['target_url_v4']
             target_url_v6 = cfg_json_item['target_url_v6']
-            now_ipv4_address = cfg_json_item['now_ipv4_address']
-            now_ipv6_address = cfg_json_item['now_ipv6_address']
+
+            v4_use_cloudflare_proxy = (
+                cfg_json_item['v4_use_cloudflare_proxy'] if cfg_json_item['v4_use_cloudflare_proxy'] != None else False)
+            v6_use_cloudflare_proxy = (
+                cfg_json_item['v6_use_cloudflare_proxy'] if cfg_json_item['v6_use_cloudflare_proxy'] != None else False)
+
             sleep_time_in_ms = cfg_json_item['sleep_time_in_ms']
 
             if api_key == "":
@@ -113,10 +117,10 @@ def load_cfg_json():
         logger.warning('Read Config File Failed --- ' + str(e))
         is_ok_flag = "Read Config File Failed"
 
-    return is_ok_flag, api_key, zone_id, target_url_v4, target_url_v6, now_ipv4_address, now_ipv6_address, sleep_time_in_ms
+    return is_ok_flag, api_key, zone_id, target_url_v4, target_url_v6, v4_use_cloudflare_proxy, v6_use_cloudflare_proxy, sleep_time_in_ms
 
 
-def save_cfg_json(new_ipv4: str, new_ipv6: str):
+def save_cfg_json():
     exe_path = os.path.split(os.path.realpath(__file__))[0]
     cfg_file_path = os.path.join(exe_path, 'config.cfgjson')
     cfg_json_item = json.loads("{}")
@@ -130,9 +134,6 @@ def save_cfg_json(new_ipv4: str, new_ipv6: str):
     except Exception as e:
         logger.warning('Read Config File Failed --- ' + str(e))
         is_ok_flag = "Read Config File Failed"
-
-    cfg_json_item['now_ipv4_address'] = new_ipv4
-    cfg_json_item['now_ipv6_address'] = new_ipv6
 
     try:
         with open(cfg_file_path, 'w') as fw:
@@ -174,8 +175,20 @@ def get_dns_record_list_from_cloudflare(zone_id: str, api_key: str):
     return res_json_item
 
 
+def check_domain_is_same(res_json_item, domain_in_cfg: str) -> bool:
+    if len(res_json_item['result']) > 0:
+        domain_in_res_json_item = res_json_item['result'][0]['zone_name']
+        domain_in_cfg_to_check = domain_in_cfg[(len(domain_in_res_json_item) * -1):]
+        if domain_in_cfg_to_check == domain_in_res_json_item:
+            return True
+        else:
+            return False
+
+
 def check_is_need_create_or_update_in_clodflare_dns_record(target_url_v4: str, now_ipv4_address: str,
+                                                           v4_use_cloudflare_proxy: bool,
                                                            target_url_v6: str, now_ipv6_address: str,
+                                                           v6_use_cloudflare_proxy: bool,
                                                            res_json_item):
     is_v4_target_url_existed = False
     is_ipv4_need_update = False
@@ -193,6 +206,9 @@ def check_is_need_create_or_update_in_clodflare_dns_record(target_url_v4: str, n
                     if i['content'] != now_ipv4_address:
                         is_ipv4_need_update = True
                         ipv4_dns_record_id = i['id']
+                    if i['proxied'] != v4_use_cloudflare_proxy:
+                        is_ipv4_need_update = True
+                        ipv4_dns_record_id = i['id']
 
         if target_url_v6 != "" and now_ipv6_address != "":
             if i['name'] == target_url_v6:
@@ -201,11 +217,15 @@ def check_is_need_create_or_update_in_clodflare_dns_record(target_url_v4: str, n
                     if i['content'] != now_ipv6_address:
                         is_ipv6_need_update = True
                         ipv6_dns_record_id = i['id']
+                    if i['proxied'] != v6_use_cloudflare_proxy:
+                        is_ipv6_need_update = True
+                        ipv6_dns_record_id = i['id']
 
     return is_v4_target_url_existed, is_ipv4_need_update, ipv4_dns_record_id, is_v6_target_url_existed, is_ipv6_need_update, ipv6_dns_record_id
 
 
-def create_dns_record_for_cloudflare(ipv4_or_ipv6: int, target_url: str, now_address: str, api_key: str, zone_id: str):
+def create_dns_record_for_cloudflare(ipv4_or_ipv6: int, target_url: str, now_address: str, use_cloudflare_proxy: bool,
+                                     api_key: str, zone_id: str):
     is_ok_flag = "OK"
 
     url = "https://api.cloudflare.com/client/v4/zones/" + zone_id + "/dns_records"
@@ -214,7 +234,7 @@ def create_dns_record_for_cloudflare(ipv4_or_ipv6: int, target_url: str, now_add
         payload = {
             "content": now_address,
             "name": target_url,
-            "proxied": True,
+            "proxied": use_cloudflare_proxy,
             "type": "A",
             "comment": "",
             "tags": [],
@@ -224,7 +244,7 @@ def create_dns_record_for_cloudflare(ipv4_or_ipv6: int, target_url: str, now_add
         payload = {
             "content": now_address,
             "name": target_url,
-            "proxied": True,
+            "proxied": use_cloudflare_proxy,
             "type": "AAAA",
             "comment": "",
             "tags": [],
@@ -246,7 +266,7 @@ def create_dns_record_for_cloudflare(ipv4_or_ipv6: int, target_url: str, now_add
 
 
 def update_dns_record_for_cloudflare(ipv4_or_ipv6: int, target_url: str, target_dns_record_id: str, now_address: str,
-                                     api_key: str, zone_id: str):
+                                     use_cloudflare_proxy: bool, api_key: str, zone_id: str):
     is_ok_flag = "OK"
 
     url = "https://api.cloudflare.com/client/v4/zones/" + zone_id + "/dns_records/" + target_dns_record_id
@@ -257,7 +277,7 @@ def update_dns_record_for_cloudflare(ipv4_or_ipv6: int, target_url: str, target_
         payload = {
             "content": now_address,
             "name": target_url,
-            "proxied": True,
+            "proxied": use_cloudflare_proxy,
             "type": "A",
             "comment": "",
             "tags": [],
@@ -267,7 +287,7 @@ def update_dns_record_for_cloudflare(ipv4_or_ipv6: int, target_url: str, target_
         payload = {
             "content": now_address,
             "name": target_url,
-            "proxied": True,
+            "proxied": use_cloudflare_proxy,
             "type": "AAAA",
             "comment": "",
             "tags": [],
@@ -289,7 +309,9 @@ def update_dns_record_for_cloudflare(ipv4_or_ipv6: int, target_url: str, target_
 
 
 if __name__ == '__main__':
-    is_ok_flag, api_key, zone_id, target_url_v4, target_url_v6, now_ipv4_address, now_ipv6_address, sleep_time_in_ms = load_cfg_json()
+    is_ok_flag, api_key, zone_id, target_url_v4, target_url_v6, v4_use_cloudflare_proxy, v6_use_cloudflare_proxy, sleep_time_in_ms = load_cfg_json()
+
+    debug_enable = 0
 
     is_loop = True
     loop_cnt = 0
@@ -298,18 +320,32 @@ if __name__ == '__main__':
         logger.info(is_ok_flag)
         is_loop = False
 
+    if debug_enable == 1:
+        is_loop = False
+        if is_ok_flag == "OK":
+            get_dns_record_list_from_cloudflare(zone_id, api_key)
+
     while is_loop:
         loop_cnt += 1
         logger.info("Start Local DDNS Checking Operation ====== Loop " + str(loop_cnt) + " ======")
 
         if loop_cnt != 1:
-            is_ok_flag, api_key, zone_id, target_url_v4, target_url_v6, now_ipv4_address, now_ipv6_address, sleep_time_in_ms = load_cfg_json()
+            is_ok_flag, api_key, zone_id, target_url_v4, target_url_v6, v4_use_cloudflare_proxy, v6_use_cloudflare_proxy, sleep_time_in_ms = load_cfg_json()
             if is_ok_flag != "OK":
                 logger.info(is_ok_flag)
                 is_loop = False
                 break
 
         res_json_item = get_dns_record_list_from_cloudflare(zone_id, api_key)
+
+        if (target_url_v4 != "") and (check_domain_is_same(res_json_item=res_json_item, domain_in_cfg=target_url_v4) is False):
+            logger.info("The IPv4 Target URL`s Domain in config.cfgjson IS NOT same with Cloudflare`s return information's Domain.")
+            is_loop = False
+            break
+        if (target_url_v6 != "") and (check_domain_is_same(res_json_item=res_json_item, domain_in_cfg=target_url_v6) is False):
+            logger.info("The IPv6 Target URL`s Domain in config.cfgjson IS NOT same with Cloudflare`s return information's Domain.")
+            is_loop = False
+            break
 
         now_ipv4_address = get_public_ip_from_ipsb(4)
         now_ipv6_address = get_public_ip_from_ipsb(6)
@@ -318,20 +354,35 @@ if __name__ == '__main__':
         logger.info("Now IPv6 Address: " + now_ipv6_address)
 
         is_v4_target_url_existed, is_ipv4_need_update, ipv4_dns_record_id, is_v6_target_url_existed, is_ipv6_need_update, ipv6_dns_record_id = \
-            check_is_need_create_or_update_in_clodflare_dns_record(target_url_v4, now_ipv4_address, target_url_v6,
-                                                                   now_ipv6_address, res_json_item)
+            check_is_need_create_or_update_in_clodflare_dns_record(target_url_v4=target_url_v4,
+                                                                   now_ipv4_address=now_ipv4_address,
+                                                                   v4_use_cloudflare_proxy=v4_use_cloudflare_proxy,
+                                                                   target_url_v6=target_url_v6,
+                                                                   now_ipv6_address=now_ipv6_address,
+                                                                   v6_use_cloudflare_proxy=v6_use_cloudflare_proxy,
+                                                                   res_json_item=res_json_item)
 
         if target_url_v4 != "":
             if is_v4_target_url_existed:
                 if is_ipv4_need_update:
                     logger.info("Public IPv4 Changed, Update......")
-                    update_dns_record_for_cloudflare(4, target_url_v4, ipv4_dns_record_id, now_ipv4_address, api_key,
-                                                     zone_id)
+                    update_dns_record_for_cloudflare(ipv4_or_ipv6=4,
+                                                     target_url=target_url_v4,
+                                                     target_dns_record_id=ipv4_dns_record_id,
+                                                     now_address=now_ipv4_address,
+                                                     use_cloudflare_proxy=v4_use_cloudflare_proxy,
+                                                     api_key=api_key,
+                                                     zone_id=zone_id)
                 else:
                     logger.info("Public IPv4 NOT Change, Pass......")
             else:
                 logger.info("IPv4 DNS Record NOT Existed, Create......")
-                create_dns_record_for_cloudflare(4, target_url_v4, now_ipv4_address, api_key, zone_id)
+                create_dns_record_for_cloudflare(ipv4_or_ipv6=4,
+                                                 target_url=target_url_v4,
+                                                 now_address=now_ipv4_address,
+                                                 use_cloudflare_proxy=v4_use_cloudflare_proxy,
+                                                 api_key=api_key,
+                                                 zone_id=zone_id)
         else:
             logger.info("No IPv4 target url, Skip IPv4......")
 
@@ -339,17 +390,27 @@ if __name__ == '__main__':
             if is_v6_target_url_existed:
                 if is_ipv6_need_update:
                     logger.info("Public IPv6 Changed, Update......")
-                    update_dns_record_for_cloudflare(6, target_url_v6, ipv6_dns_record_id, now_ipv6_address, api_key,
-                                                     zone_id)
+                    update_dns_record_for_cloudflare(ipv4_or_ipv6=6,
+                                                     target_url=target_url_v6,
+                                                     target_dns_record_id=ipv6_dns_record_id,
+                                                     now_address=now_ipv6_address,
+                                                     use_cloudflare_proxy=v6_use_cloudflare_proxy,
+                                                     api_key=api_key,
+                                                     zone_id=zone_id)
                 else:
                     logger.info("Public IPv6 NOT Change, Pass......")
             else:
                 logger.info("IPv6 DNS Record NOT Existed, Create......")
-                create_dns_record_for_cloudflare(6, target_url_v6, now_ipv6_address, api_key, zone_id)
+                create_dns_record_for_cloudflare(ipv4_or_ipv6=6,
+                                                 target_url=target_url_v6,
+                                                 now_address=now_ipv6_address,
+                                                 use_cloudflare_proxy=v6_use_cloudflare_proxy,
+                                                 api_key=api_key,
+                                                 zone_id=zone_id)
         else:
             logger.info("No IPv6 target url, Skip IPv6......")
 
-        save_cfg_json(now_ipv4_address, now_ipv6_address)
+        # save_cfg_json()
 
         logger.info("Operation Finished. Sleep " + str(sleep_time_in_ms / 1000) + " Sec......\n\n")
         sleep(sleep_time_in_ms / 1000)
